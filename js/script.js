2a7a799c57f366ca3e51807f0a3657e93a24f74c
Aditya_Kumar <aditya.2125it1166@kiet.edu>
2023-06-21 20:05:39 +0530

    inital commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+function login(){
+    window.alert("Successfully Login......");
+}
+function register(){
+    window.alert("Successfuly Signed in.......");
+}
